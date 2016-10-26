61f8d84f708d7c9cc16f208cffbf87eb51e4c5e5
Felix Eichner <eichner.f@googlemail.com>
2016-10-26 14:34:33 +0000

    create style and ingredients display and user interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:danger] = "You need to be logged in!"
-      redirect_to :back
+      redirect_to recipes_path
     end
   end
   
